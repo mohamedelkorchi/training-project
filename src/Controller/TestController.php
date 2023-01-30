5c34dbb06c908b3028bcae134639823b790d72c8
mohamedelkorchi <mohamed__elkorchi@hotmail.fr>
2023-01-30 10:19:12 +0100

    debut projet

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class TestController
 {
     public function index()
@@ -10,10 +14,29 @@ class TestController
         die();
     }
 
-    public function test()
+
+    /*
+    * @Route("/test/{age<\d+>?0}", name="test")
+    */
+    public function test(Request $request, $age) // reprend la ligne 19
     {
         // dump("page test");
-        dd("page test");  // dd signifie DUMP AND DIE           
+        // $request = Request::createFromGlobals();
+
+        // dump($request);
+
+        // $age = $request->attributes->get("age"); // remplace le code avec le if pour l'age 
+
+        return new Response("Vous avez $age ans");
+
+        // $age = 0;
+
+        // if (!empty($_GET['age'])) {          // mettre age =0 et le if pour pouvoir prendre lage sans le mettre dans lurl
+
+        //     $age = $_GET['age'];
+        // }
+
+        dd("t'as $age ans");  // dd signifie DUMP AND DIE           
        
     }    
 
